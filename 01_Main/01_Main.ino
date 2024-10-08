8be44e5cf4cf076218ae6e0dc46bbdf8df25cef5
kai jie <leowkaijie@gmail.com>
2024-10-08 15:10:21 +0800

    Clean up code

--- 01_Main/01_Main.ino
+++ 01_Main/01_Main.ino
@@ -34,11 +34,7 @@ int servoCounter = 0;
 // 05_Ui
 void setupUi();
 void readButtonInput();
-int buttonReading = 1;      // Reading of button
-int buttonState = 1;        // Variable to hold the state of the button
-int lastButtonState = 1;    // Variable to store the last button state  
-const int debounceDelay = 50;  // Debouncing time in milliseconds
-unsigned long lastDebounceTime = 0;  // Last time the button state changed
+
 
 // 06_Fan
 
@@ -52,6 +48,8 @@ void setup() {
   // Initialize the serial port:
   Serial.begin(9600);
 
+  Serial.println();
+  Serial.println("===============");
   Serial.println("Set up complete");
 
 }
@@ -59,18 +57,6 @@ void setup() {
 void loop() {
   // Handle the current state
   handleState();
-  // readButtonInput();
-  // buttonState = digitalRead(BUTTONPIN); // Read the current state of the button (LOW when pressed, HIGH when not pressed)
-    
-  // // Check if the button state has changed (button press detected)
-  // if (buttonState != lastButtonState) {
-  //   if (buttonState == LOW) {
-  //     buttonState = 0;
-  //     // Button is pressed
-  //     Serial.println("Button Pressed");
-
-  // Move to wash compartment. step one revolution in one direction:
-  
 }
 
 // Function to handle the logic for each state
@@ -81,7 +67,7 @@ void handleState() {
       Serial.println("State 1 - Await Button Press");
   
       readButtonInput();
-      // digitalWrite(ledPin, LOW);
+      
       break;
     case STATE_2:
       // Handle logic for STATE_2
@@ -104,8 +90,7 @@ void handleState() {
     case STATE_3:
       Serial.println("State 3 - Start Wash");
       delay(3000); // washing starts
-    
-      // Additional code here
+  
       break;
     case STATE_4:
       Serial.println("State 4 - Drying Cycle Setup");
@@ -153,12 +138,12 @@ void handleState() {
       Serial.print("State Number: ");
       Serial.println(currentState);
       // Handle logic for STATE_15
-      // Additional code here
+      
       currentState = STATE_15;  // Reset to initial state
       break;
     case STATE_6:
       // Handle logic for STATE_15
-      // Additional code here
+      
       currentState = STATE_15;  // Reset to initial state
       break;
     case STATE_7:
@@ -230,217 +215,3 @@ void changeState() {
       break;
   }
 }
-
-
-
-  
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  /*
-
-  void loop() 
-  {
-    buttonState = digitalRead(BUTTONPIN); // Read the current state of the button (LOW when pressed, HIGH when not pressed)
-    
-    // Check if the button state has changed (button press detected)
-    if (buttonState != lastButtonState) {
-      if (buttonState == LOW) {
-        buttonState = 0;
-        // Button is pressed
-        Serial.println("Button Pressed");
-
-        // Move to wash compartment. step one revolution in one direction:
-        Serial.println("h stepper right");
-        railStepper.step(stepsPerRevolution * totalRevolutionsForMovement);
-        delay(3000);
-
-        Serial.println("Servo start spinning");
-        timer = millis();
-        while ((millis()-timer) < timeout) {
-          // Serial.println(servoCounter);
-          cupServo.write(60);
-        }
-        cupServo.write(90);
-        delay(3000); // washing starts
-
-
-        // step one revolution in the other direction:
-        Serial.println("h stepper left");
-        railStepper.step(stepsPerRevolution * totalRevolutionsForMovement);
-        delay(3000);
-
-        
-      
-        Serial.println("v stepper up ");
-        fanStepper.step(-stepsPerRevolution * totalRevolutionsForMovement);
-        delay(3000); 
-
-        Serial.println("Servo start spinning");
-        timer = millis();
-        while ((millis()-timer) < timeout) {
-          // Serial.println(servoCounter);
-          cupServo.write(60);
-        }
-        
-        cupServo.write(90);
-        delay(3000); // washing starts
-
-        // fan start
-        digitalWrite(FANRELAYPIN, HIGH);
-        delay(5000); // fan blows for 5 seconds
-
-        // cup stop, fan stop
-        digitalWrite(FANRELAYPIN, LOW);
-
-        Serial.println("v stepper down ");
-        fanStepper.step(stepsPerRevolution * totalRevolutionsForMovement);
-        delay(3000); 
-
-      } else {
-        Serial.println("Button Not Pressed");
-      }
-      // Small delay to debounce the button
-      delay(50);
-    }
-
-    // Save the current button state as the last state for the next iteration
-    lastButtonState = buttonState;
-  }
-*/
-
-/*
-// Pin definitions
-const int buttonPin = 7;  // Button input
-const int ledPin = 13;    // LED output
-
-// Define 15 states for the finite state machine
-enum State {
-  STATE_1, STATE_2, STATE_3, STATE_4, STATE_5, STATE_6, STATE_7, STATE_8,
-  STATE_9, STATE_10, STATE_11, STATE_12, STATE_13, STATE_14, STATE_15
-};
-
-// Initial state
-State currentState = STATE_1;
-
-// Variables for button press
-int buttonState = LOW;
-int lastButtonState = LOW;
-unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 50;  // Debounce delay for button
-
-void setup() {
-  pinMode(buttonPin, INPUT);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);  // Initially turn off the LED
-}
-
-void loop() {
-  // Read the button state
-  int reading = digitalRead(buttonPin);
-
-  // Debounce the button input
-  if (reading != lastButtonState) {
-    lastDebounceTime = millis();
-  }
-
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (reading != buttonState) {
-      buttonState = reading;
-
-      // If the button is pressed, change state
-      if (buttonState == HIGH) {
-        changeState();
-      }
-    }
-  }
-
-  lastButtonState = reading;
-
-  // Handle the current state
-  handleState();
-}
-
-// Function to handle the logic for each state
-void handleState() {
-  switch (currentState) {
-    case STATE_1:
-      // Handle logic for STATE_1
-      digitalWrite(ledPin, LOW);
-      break;
-    case STATE_2:
-      // Handle logic for STATE_2
-      digitalWrite(ledPin, HIGH);
-      break;
-    case STATE_3:
-      // Handle logic for STATE_3
-      // Additional code here
-      break;
-    case STATE_4:
-      // Handle logic for STATE_4
-      // Additional code here
-      break;
-    // Add more cases for STATE_5 to STATE_15
-    case STATE_15:
-      // Handle logic for STATE_15
-      // Additional code here
-      break;
-    default:
-      // Handle unknown state
-      currentState = STATE_1;  // Reset to initial state
-      break;
-  }
-}
-
-// Function to change the state
-void changeState() {
-  // Cycle through states: STATE_1 -> STATE_2 -> STATE_3 -> ... -> STATE_15 -> STATE_1
-  switch (currentState) {
-    case STATE_1:
-      currentState = STATE_2;
-      break;
-    case STATE_2:
-      currentState = STATE_3;
-      break;
-    case STATE_3:
-      currentState = STATE_4;
-      break;
-    case STATE_4:
-      currentState = STATE_5;
-      break;
-    case STATE_5:
-      currentState = STATE_6;
-      break;
-    case STATE_6:
-      currentState = STATE_7;
-      break;
-    case STATE_7:
-      currentState = STATE_8;
-      break;
-    case STATE_8:
-      currentState = STATE_9;
-      break;
-    case STATE_9:
-      currentState = STATE_10;
-      break;
-    case STATE_10:
-      currentState = STATE_11;
-      break;
-    case STATE_11:
-      currentState = STATE_12;
-      break;
-    case STATE_12:
-      currentState = STATE_13;
-      break;
-    case STATE_13:
-      currentState = STATE_14;
-      break;
-    case STATE_14:
-      currentState = STATE_15;
-      break;
-    case STATE_15:
-      currentState = STATE_1;
-      break;
-  }
-}
-
-
-*/
--- 01_Main/05_Ui.ino
+++ 01_Main/05_Ui.ino
@@ -1,3 +1,9 @@
+// Set up variables
+int buttonState = 1;        // Variable to hold the state of the button
+int lastButtonState = 1;    // Variable to store the last button state  
+const int debounceDelay = 50;  // Debouncing time in milliseconds
+unsigned long lastDebounceTime = 0;  // Last time the button state changed
+
 void setupUi() {
   // Button Setup
   pinMode(BUTTONPIN, INPUT_PULLUP);  // Enable the internal pull-up resistor for the button  
