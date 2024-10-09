554ada0aaf59256863545e54a1edd2728611a921
kai jie <leowkaijie@gmail.com>
2024-10-09 20:37:51 +0800

    Test servo

--- Archives/cup-servo-motor/cup-servo-motor.ino
+++ Archives/cup-servo-motor/cup-servo-motor.ino
@@ -1,19 +1,39 @@
+// #include <Servo.h>
+
+// Servo myservo;  // create servo object to control a servo
+
+
+// void setup() {
+//   myservo.attach(3);  // attaches the servo on pin 9 to the servo object
+// }
+
+// void loop() {
+
+//   // 0: full speed clockwise8
+//   // 90: stop
+//   // 180: full speed counter clockwise
+ 
+//   myservo.write(60);                  // sets the servo position according to the scaled value
+//   // delay(1000);                           // waits for the servo to get there
+//   // myservo.write(180);
+// }
+
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-
+Servo myServo; // Create a Servo object to control the servo
 
 void setup() {
-  myservo.attach(3);  // attaches the servo on pin 9 to the servo object
+  myServo.attach(4); // Attach the servo to pin 9 on the Arduino
 }
 
 void loop() {
+  Serial.println("Spinnning");
+  myServo.write(60);  // Set servo to the neutral position (stops if it's a continuous servo)
+  // delay(2000);        // Wait for 2 seconds
 
-  // 0: full speed clockwise8
-  // 90: stop
-  // 180: full speed counter clockwise
- 
-  myservo.write(60);                  // sets the servo position according to the scaled value
-  // delay(1000);                           // waits for the servo to get there
-  // myservo.write(180);
+  // myServo.write(180); // Rotate the servo clockwise
+  // delay(2000);        // Wait for 2 seconds
+
+  // myServo.write(0);   // Rotate the servo counterclockwise
+  // delay(2000);        // Wait for 2 seconds
 }
