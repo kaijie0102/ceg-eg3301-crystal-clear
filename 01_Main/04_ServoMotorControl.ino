d542daf451229e0ab5e218c7c32c1af1d1ddbe97
kai jie <leowkaijie@gmail.com>
2024-10-11 09:58:10 +0800

    Remove comments

--- 01_Main/04_ServoMotorControl.ino
+++ 01_Main/04_ServoMotorControl.ino
@@ -1,7 +1,7 @@
-// Servo cupServo;  // create continuous servo object
-unsigned long timer; 
-static const unsigned long timeout = 7500; // loop ends after specified duration
-int servoCounter = 0;  
+Servo cupServo;  // create continuous servo object
+// unsigned long timer; 
+// static const unsigned long timeout = 7500; // loop ends after specified duration
+// int servoCounter = 0;  
 
 void setupServoMotor() {
   // Servo Motor Setup
