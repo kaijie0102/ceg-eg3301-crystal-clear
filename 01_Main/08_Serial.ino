d1af2c84af9472601274fde0d6891aace7f29ede
kai jie <leowkaijie@gmail.com>
2024-11-05 15:40:22 +0800

    Add serial comms with Arduino Nano

---
+++ 01_Main/08_Serial.ino
@@ -0,0 +1,39 @@
+// Set up SoftwareSerial on pins 10 and 11
+SoftwareSerial mySerial(41, 43); // RX, TX
+
+void setupSerial() {
+  // Initialize SoftwareSerial at 9600 bps for communication with the Nano
+  mySerial.begin(9600);
+}
+
+void ledStandbyMode() {
+  mySerial.println(1);
+}
+
+void ledWashMode() {
+  mySerial.println(2);
+}
+
+void ledDryMode() {
+  mySerial.println(3);
+}
+
+void ledCompleteMode() {
+  mySerial.println(4);
+}
+
+void performSerial(){
+  // Send a message to Nano
+  mySerial.println(1);
+  delay(8000); 
+
+  mySerial.println(2);
+  delay(5000); 
+
+  mySerial.println(3);
+  delay(5000); 
+
+  mySerial.println(4);
+  delay(5000); 
+}
+
